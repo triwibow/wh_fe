b1c43f27b18e8edd8c57753c3f6cabcba47c98f7
triwibow <triw3707@gmail.com>
2022-08-31 13:56:29 +0700

    add new endpoint

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "https://api-wh.herokuapp.com/api/v1"
+    baseURL: "https://wayshub.herokuapp.com/api/v1"
 });
 
 const setAuthToken = (token) => {
